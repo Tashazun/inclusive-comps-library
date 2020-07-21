4de6ffc2859733e28948168dc19e30df6a2c8899
Tasha Zuniga <tashazun@gmail.com>
2020-07-20 21:11:50 -0700

    slider comp start

---
+++ src/components/sliders/Slider.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./Slider.scss";
+
+const Slider = ({ text, style }) => {
+return
+  <div>
+    <label>Slider</label>
+    <input
+      type="range"
+      min="1"
+      max="4"
+      value="1"
+      step="1"
+      aria-label="Slider Count"
+      aria-valuetext="value: 1" />
+  </div>;
+}
+
+export default Slider
